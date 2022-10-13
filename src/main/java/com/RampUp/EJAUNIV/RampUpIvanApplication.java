12ace578ac07169eeb716040014c22ba00b4fec6
Ivanjrr <Ivanjrr99@gmail.com>
2022-10-13 08:22:18 -0300

    Project Created

---
+++ src/main/java/com/RampUp/EJAUNIV/RampUpIvanApplication.java
@@ -0,0 +1,13 @@
+package com.RampUp.EJAUNIV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RampUpIvanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RampUpIvanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RampUp/RampUp/RampUpIvanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RampUp.RampUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RampUpIvanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
